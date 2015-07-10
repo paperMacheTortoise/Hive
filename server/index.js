e2735c8238245e3d2865587de78f6004020fb98f
Austin Worachet <wworachet@gmail.com>
2015-07-10 10:53:04 -0700

    (feat) basic server setup closed #3

---
+++ server/index.js
@@ -0,0 +1,13 @@
+var Firebase = require('firebase');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json())
+  .use(express.static(__dirname + './../client'));
+
+app.listen(port);
+console.log('BizGram magic being delivered on', port);
