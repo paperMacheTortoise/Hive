ebb584a515037876f2e2c932989979017e92f128
Danielle Blank <dgdblank@gmail.com>
2015-07-11 12:13:13 -0700

    (bug) debugging why not retrieving data from database

--- client/app/factories/factories.js
+++ client/app/factories/factories.js
@@ -2,13 +2,14 @@ angular.module('bizGramFactories', ['firebase'])
 
 .factory('Rooms', ['$firebaseArray', function ($firebaseArray){
 
-	var ref = new Firebase ('https://bizgramer.firebaseio.com/hr/messages');
+	var roomsFactory = {};
+	var ref = new Firebase('https://bizgramer.firebaseio.com/hr/messages');
 	var messages = $firebaseArray(ref);
 
 	// create a rooms object to store the room names. Loop through the messages,
 	// and return all the room names. Storing the rooms in an object will automatically
 	// remove any duplicate room names.
-	var getRooms = function(){
+	roomsFactory.getRooms = function(){
 		var rooms = {};
 		for(var i = 0; i < messages.length; i++){
 			rooms[messages[i].room] = null;
@@ -18,18 +19,20 @@ angular.module('bizGramFactories', ['firebase'])
 		return Object.keys(rooms);
 	};
 
-	return {
-		getRooms: getRooms
-	};
+	return roomsFactory;
 }])
 
 .factory('Users', ['$firebaseArray', function ($firebaseArray){
 
-	var ref = new Firebase ('https://bizgramer.firebaseio.com/hr/users');
+
+	var userFactory = {};
+	var ref = new Firebase('https://bizgramer.firebaseio.com/hr/users');
 	var users = $firebaseArray(ref);
 
 	// parse the usernames from the database
-	var getUsers = function(){
+	userFactory.getUsers = function(){
+		debugger;
+		console.log(users);
 		var usernames = [];
 		for(var i = 0; i < users.length; i++){
 			usernames.push(users[i].username);
@@ -39,11 +42,11 @@ angular.module('bizGramFactories', ['firebase'])
 		return usernames;
 	};
 
-	return {
-		getUsers: getUsers
-	};
+	return userFactory;
 }])
+
 .factory('Auth',['$firebaseAuth',function($firebaseAuth){
+	
 	var authFactory = {};
 	var ref = new Firebase('https://bizgramer.firebaseio.com/');
 
@@ -83,4 +86,5 @@ angular.module('bizGramFactories', ['firebase'])
     signin: signin,
     signup: signup
   };
+
 }]);
