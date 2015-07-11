e6a7b71ac9bfb33b2c09276e157d48f41fc6d099
Austin Worachet <wworachet@gmail.com>
2015-07-11 12:04:59 -0700

    (fix) merge conflict on angular controller

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -3,14 +3,6 @@ angular.module('mainCtrl', [])
 .controller('mainController', function (Rooms, Users) {
 
 	var vm = this;
-		vm.rooms = Rooms.getRooms();
-		vm.users = Users.getUsers();
-
-	vm.getUsers = function(){
-		Users.getUsers()
-			.then(function(users){
-				vm.users =  users;
-			});
-	};
-	vm.getUsers();
+	vm.rooms = Rooms.getRooms();
+	vm.users = Users.getUsers();
 });
