1a8cc9da6772020df060784f54d19d9a2091d6b9
Austin Worachet <wworachet@gmail.com>
2015-07-13 15:05:44 -0700

    (feat) roomController for chat submit

--- client/app/room/room.js
+++ client/app/room/room.js
@@ -1,10 +1,32 @@
 // Angular controller for room.html
+console.log('room.js');
 angular.module('roomCtrl', [])
 
-.controller('roomController', function () {
+.controller('roomController', ["$firebaseArray", function ($firebaseArray) {
 
   var vm = this;
 
-});
+  this.addMessage = function (e) {
+    if (e.keyCode === 13 && vm.text) {
+      var userName = vm.username || 'anon';
+      var roomName = vm.roomname || 'general';
+      console.log(userName, roomName, vm.text);
+
+    var url = "https://bizgramer.firebaseio.com/hr/rooms/" + roomName;
+    console.log(url);
+    var ref = new Firebase(url);
+
+    vm.allMessages = $firebaseArray(ref);
+
+      vm.allMessages.$add({
+        username: userName,
+        // room: roomName,
+        text: vm.text
+      });
+
+      vm.text = '';
+    }
+  }
+}]);
 
 
