3aaac64e7f38e544ee97c2f596c792862aa3e380
Austin Worachet <wworachet@gmail.com>
2015-07-13 11:31:28 -0700

    (feat) chat subview html closed #59

---
+++ client/app/room/room.js
@@ -0,0 +1,16 @@
+// Angular controller for room.html
+angular.module('mainCtrl', [])
+
+.controller('mainController', function (Rooms, Users) {
+
+  var vm = this;
+  vm.rooms = Rooms.getRooms();
+  vm.users = Users.getUsers();
+  vm.roomName = "General";
+
+  vm.getRoom = function(index){
+    vm.roomName = vm.rooms[index].name;
+  };
+});
+
+
