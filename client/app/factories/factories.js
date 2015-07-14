98902cf13686a4a05065b21af4b20e86bbac783b
Liam Gallivan <gallivanster@gmail.com>
2015-07-14 12:03:46 -0700

    (feat) CDN for images, allows uploading pictures

--- client/app/factories/factories.js
+++ client/app/factories/factories.js
@@ -49,9 +49,26 @@ angular.module('bizGramFactories', ['firebase'])
 	return roomsFactory;
 }])
 .factory('Upload',['$firebaseArray', function ($firebaseArray){
+  var uploadFactory={};
 	AWS.config.update({accessKeyId: 'AKIAIVEO6DBRV7OF7YDA', secretAccessKey: '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'});
-	AWS.config.region = 'us-west-1';
-	var s3 = new AWS.S3();
+	AWS.config.region = 'us-west-2';
+	var s3 = new AWS.S3({params:{Bucket:'bizgram'}});
+  uploadFactory.putFile = function(body, name,callback){
+    s3.upload({
+      Body: body,
+      Key: 'images/'+name,
+      ACL: "public-read-write"
+    },function(err,data){
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log(data);
+        callback('https://drgvq3mghnaxi.cloudfront.net/images/'+name);
+      }
+    });
+  };
+  return uploadFactory;
 }])
 
 .factory('Users', ['$firebaseArray', '$firebaseObject', function ($firebaseArray, $firebaseObject){
