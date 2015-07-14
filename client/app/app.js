21d9f87bebf2a5e13719e2b2db4b937ce2298e04
Danielle Blank <dgdblank@gmail.com>
2015-07-13 22:27:46 -0700

    (fix) cleaned app.js main.room state

--- client/app/app.js
+++ client/app/app.js
@@ -15,12 +15,7 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 	.state('main.room', {
 		url: 'room/:roomName',
 		parent: 'main',
-		templateUrl: 'app/room/room.html',
-		controller: 'roomController',
-		params: {roomName: 'value'}
-		// controller: function($stateParams) {
-		// 	vm.roomName = $stateParams.roomName;
-		// }
+		templateUrl: 'app/room/room.html'
 	})
 	.state('main.direct', {
 		url: '/dm',
