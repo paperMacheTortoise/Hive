0590d37109f7e4018dfaa364938a824ab67b8f9e
Austin Worachet <wworachet@gmail.com>
2015-07-14 19:10:38 -0700

    (feat) ng-replyController for message replying closed #77

--- client/app/room/reply.js
+++ client/app/room/reply.js
@@ -11,6 +11,17 @@ angular.module('replyCtrl', [])
     this.isReplying = !(this.isReplying);
   };
 
+  // add reply to the current message
+  vm.addReply = function (e, index) {
+    if (e.keyCode === 13) {
+      var roomName = Rooms.getCurrentName();
+      var userName = vm.replyusername || 'anon';
+      console.log(userName, vm.replytext, index, roomName);
+      Replies.addReply(userName, vm.replytext, index, roomName);
+      vm.replytext = '';
+    }
+  };
+
 });
 
 
