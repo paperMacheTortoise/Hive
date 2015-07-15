a0ee4db15a15e7e6a6550d69245bdc8c32d5292b
Austin Worachet <wworachet@gmail.com>
2015-07-14 19:09:45 -0700

    (feat) ng-replyController to toggle showing reply input box

--- client/app/room/reply.js
+++ client/app/room/reply.js
@@ -3,6 +3,14 @@ angular.module('replyCtrl', [])
 
 .controller('replyController', function (Replies, Rooms) {
 
+  var vm = this;
+  vm.isReplying = false;
+
+  // toggle to show the reply input box
+  vm.toggleReplying = function() {
+    this.isReplying = !(this.isReplying);
+  };
+
 });
 
 
