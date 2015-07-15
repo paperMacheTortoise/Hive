6df79008dd78e517b74febb727a7a39e1392317f
Austin Worachet <wworachet@gmail.com>
2015-07-15 11:42:55 -0700

    (feat) ng-controller for adding new room

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,6 +1,6 @@
 angular.module('mainCtrl', [])
 
-.controller('mainController', function (Rooms, Users, $rootScope, $state) {
+.controller('mainController', function (Rooms, Users) {
 
 	var vm = this;
 	vm.rooms = Rooms.getRooms();
@@ -9,5 +9,19 @@ angular.module('mainCtrl', [])
 	vm.setRoom = function(index){
 		Rooms.setRoom(vm.rooms[index]);
 	};
+
+  vm.addRoom = function(e) {
+    if (e.keyCode === 13) {
+      var nameOfRoomToAdd = vm.nameOfRoomToAdd || 'new room';
+      if (vm.rooms.indexOf(nameOfRoomToAdd) === -1) {
+        console.log('Adding room ', nameOfRoomToAdd);
+        Rooms.addRoom(nameOfRoomToAdd);
+        vm.nameOfRoomToAdd = '';
+      } else {
+        console.log('this room already exists');
+      }
+    }
+  };
+
 });
 
