fcc3c634dbc45f5df6f2eac828e246ddde41de70
Austin Worachet <wworachet@gmail.com>
2015-07-16 14:54:01 -0700

    (feat) org names available in orgsignup factory

--- client/app/factories/orgsignupFactory.js
+++ client/app/factories/orgsignupFactory.js
@@ -2,4 +2,17 @@ angular.module('orgsignupFactory', ['firebase'])
 
 .factory('OrgSignup', ['$firebaseArray', function ($firebaseArray) {
 
+  var orgsignupFactory = {};
+  var ref = new Firebase('https://bizgramer.firebaseio.com/');
+  var organizations = $firebaseArray(ref);
+  var orgNames = [];
+
+  organizations.$loaded()
+    .then(function () {
+      angular.forEach(organizations, function (org) {
+        orgNames.push(org.$id);
+      });
+      console.log(orgNames);
+    });
+
 }]);
