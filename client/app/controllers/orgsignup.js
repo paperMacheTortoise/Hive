b9a698c75f7924d9741d864c6e14f55e15a99be9
Austin Worachet <wworachet@gmail.com>
2015-07-16 14:46:56 -0700

    (feat) org signup controller

---
+++ client/app/controllers/orgsignup.js
@@ -0,0 +1,9 @@
+angular.module('orgsignupCtrl', ['firebase'])
+
+  .controller('OrgsignupController', function () {
+    var vm = this;
+
+    vm.signuporg = function (orgname) {
+      // orgsignup factory to get the firebase array
+    }
+  })
