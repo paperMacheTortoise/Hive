151c917b3cbe517a4070d30dcae96c1b140855eb
Austin Worachet <wworachet@gmail.com>
2015-07-17 16:39:42 -0700

    (fix) index controller handing org property for routing

--- client/app/controllers/index.js
+++ client/app/controllers/index.js
@@ -3,7 +3,11 @@ angular.module('indexCtrl', ['firebase', ])
   .controller('IndexController', function ($state, $firebaseAuth, Auth, Users, $rootScope, $stateParams) {
 
     var vm = this;
-    vm.org = $stateParams.org;
+
+    // when the state changes, set the org property to the current location for routing purpose
+    $rootScope.$on('$stateChangeSuccess', function() {
+      vm.org = $stateParams.org;
+    });
 
     vm.logout = function () {
       $rootScope.logInfo = null;
