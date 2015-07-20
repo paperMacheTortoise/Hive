a0109f7e7f287aa3bfb5548d7c6bc328a678c0de
Austin Worachet <wworachet@gmail.com>
2015-07-20 15:10:54 -0700

    (feat) auth factory for org key signing up

--- client/app/factories/authFactory.js
+++ client/app/factories/authFactory.js
@@ -1,16 +1,16 @@
 angular.module('authFactory', ['firebase'])
 
-.factory('Auth', ['$firebaseAuth', function ($firebaseAuth){
+.factory('Auth', ['$firebaseAuth', '$firebaseArray', function ($firebaseAuth, $firebaseArray){
 
 	var authFactory = {};
 	var ref = new Firebase('https://bizgramer.firebaseio.com/');
 
 	var authObj = $firebaseAuth(ref);
-	authFactory.getAuth= function(callback){
-		var authData = authObj.$getAuth();
-		console.log(authData);
-		callback(authData);
-	};
+  authFactory.getAuth= function(callback){
+    var authData = authObj.$getAuth();
+    console.log(authData);
+    callback(authData);
+  };
 
   authFactory.signin = function(email,password,callback){
     authObj.$authWithPassword({
@@ -25,16 +25,28 @@ angular.module('authFactory', ['firebase'])
     });
   };
 
-	authFactory.signup = function(email,password, callback, vm){
-    authObj.$createUser({
-      email: email,
-      password: password
-    }).then(function(userData){
-      console.log("User " + userData.uid + " created successfully!");
-      authFactory.signin(email,password,callback,vm);
-    }).catch(function(error){
-      console.log('Error:',error);
-    });
+  authFactory.signup = function(email, password, orgId, orgName, callback, vm){
+    var orgRef = new Firebase('https://bizgramer.firebaseio.com/'+orgName)
+    var orgArr = $firebaseArray(orgRef);
+    orgArr.$loaded()
+      .then(function() {
+        if(orgArr.$getRecord('orgKey')['$value'] === orgId) {
+          authObj.$createUser({
+            email: email,
+            password: password
+          }).then(function(userData){
+            console.log("User " + userData.uid + " created successfully!");
+            authFactory.signin(email,password,callback,vm);
+          }).catch(function(error){
+            console.log('Error:',error);
+          });
+        } else {
+          console.log('incorrect Id for ', orgName);
+        }
+      })
+      .catch(function (error) {
+        console.log('error', error);
+      });
 	};
 
 	authFactory.signout = function(){
