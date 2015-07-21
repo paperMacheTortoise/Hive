38a39dc2e65d2a5a753127fd011a5211bfb0ce81
Danielle Blank <dgdblank@gmail.com>
2015-07-20 19:47:15 -0700

    (feat) styles

--- client/app/controllers/main.js
+++ client/app/controllers/main.js
@@ -6,12 +6,12 @@ angular.module('mainCtrl', [])
 
   vm.org = $stateParams.org;
   if ($rootScope && $rootScope.logInfo) {
-    vm.currentUser = $rootScope.logInfo.username;
+    vm.currentUser = $rootScope.logInfo;
   }
   // Get the rooms from the roomFactory.
   vm.rooms = Rooms.getRooms(vm.org);
   // Get all users except for the current user from the userFactory.
-  vm.users = Users.getDisplayUsers(vm.currentUser, vm.org);
+  vm.users = Users.getDisplayUsers(vm.currentUser.username, vm.org);
   // Get all visuals from the visualFactory.
   vm.visuals = Visualization.getVisualNames(vm.org);
 
