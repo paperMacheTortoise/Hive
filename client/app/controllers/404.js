5ad7c222cad5318a649499f70ca73cf220f83827
Austin Worachet <wworachet@gmail.com>
2015-07-21 11:20:23 -0700

    (feat) load controller

---
+++ client/app/controllers/404.js
@@ -0,0 +1,19 @@
+angular.module('404Ctrl', ['firebase'])
+
+    .controller('404Controller', function ($state, $firebaseAuth, Auth, Users, $rootScope, $stateParams) {
+
+        var vm = this;
+
+        // when the state changes, set the org property to the current location for routing purpose
+        $rootScope.$on('$stateChangeSuccess', function() {
+            vm.org = $stateParams.org;
+        });
+
+        // logout and redirect to landing page
+        vm.goToLanding = function() {
+            $rootScope.logInfo = null;
+            Auth.signout();
+            $state.go('landing');
+        };
+
+    });
