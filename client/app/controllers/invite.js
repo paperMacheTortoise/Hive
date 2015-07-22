673287036783a9554dab3c984ae077b4960a45dd
Austin Worachet <wworachet@gmail.com>
2015-07-21 19:25:45 -0700

    (feat) add invite controller file

---
+++ client/app/controllers/invite.js
@@ -0,0 +1,7 @@
+angular.module('inviteCtrl', ['firebase'])
+
+  .controller('InviteController', function ($state, $firebaseAuth, Auth, Users, $rootScope, $stateParams) {
+
+    var vm = this;
+
+  });
