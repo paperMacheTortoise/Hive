350c7ce902c60ee6b55ae3dedc232e0ab267564d
Austin Worachet <wworachet@gmail.com>
2015-07-21 19:31:13 -0700

    (feat) invite controller structure

--- client/app/controllers/invite.js
+++ client/app/controllers/invite.js
@@ -4,4 +4,13 @@ angular.module('inviteCtrl', ['firebase'])
 
     var vm = this;
 
+    // when the state changes, set the org property to the current location for routing purpose
+    $rootScope.$on('$stateChangeSuccess', function() {
+      vm.org = $stateParams.org;
+    });
+
+    vm.emailInvite = function () {
+
+    };
+
   });
