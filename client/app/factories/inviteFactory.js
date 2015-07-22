3f8e25436c39b15e5bff7ac5690438e606e991a9
Austin Worachet <wworachet@gmail.com>
2015-07-21 19:31:26 -0700

    (feat) invite factory structure

---
+++ client/app/factories/inviteFactory.js
@@ -0,0 +1,13 @@
+angular.module('inviteFactory', ['firebase'])
+
+.factory('invite', ['$firebaseArray', function ($firebaseArray) {
+
+  var inviteFactory = {};
+
+  inviteFactory.sendEmailInvitation = function () {
+
+  };
+
+  return orgsignupFactory;
+
+}]);
