de2960c4258b412304cf116a3637688fb5428b10
Austin Worachet <wworachet@gmail.com>
2015-07-23 15:28:01 -0700

    (test) initialize inviteFactory test

--- specs/client/inviteFactory.spec.js
+++ specs/client/inviteFactory.spec.js
@@ -1,4 +1,10 @@
 describe('inviteFactory', function(){
   var invite, $firebaseObject;
 
+  beforeEach(module('inviteFactory'));
+  beforeEach(inject(function(_invite_, _$firebaseObject_, _$http_){
+    invite = _invite_;
+    $firebaseObject = _$firebaseObject_;
+  }));
+
 });
