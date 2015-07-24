2717ccd59c9437082d3b4e1d8c2fa4350563c540
Liam Gallivan <gallivanster@gmail.com>
2015-07-24 15:13:26 -0700

    (fix) linting

--- client/app/factories/uploadFactory.js
+++ client/app/factories/uploadFactory.js
@@ -1,13 +1,13 @@
 angular.module('uploadFactory', ['ngImgur'])
 
-.factory('Upload', function (imgur, Users, $rootScope){
+.factory('Upload', function (imgur){
   var uploadFactory={};
 
   uploadFactory.putFile = function(file, filename, callback){
   	imgur.setAPIKey('Client-ID 83d05ca770c0014');
   	imgur.upload(file).then(function(data){
   		callback(data.link);
-  	})
+  	});
   };
 
   return uploadFactory;
