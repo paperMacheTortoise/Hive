cf2b63bb6bb1320bcc52331e38a07e8ca58b3b12
Austin Worachet <wworachet@gmail.com>
2015-07-30 16:06:27 -0700

    (cleanup)

--- client/app/factories/roomFactory.js
+++ client/app/factories/roomFactory.js
@@ -61,16 +61,11 @@ angular.module('roomFactory', ['firebase'])
       text: text,
       createdAt: Firebase.ServerValue.TIMESTAMP
     });
-      // var id = roomRef.key();
-      // console.log('added a message with id ', id); // eg. -JuDu4oKDL_nl3hBPaOP
-      // console.log('location in the array ', messages.$indexFor(id)); // eg. 3
-    // });
   };
 
   // Adds a new room to the db.
   roomsFactory.addRoom = function (roomname, org) {
     var url = "https://bizgramer.firebaseio.com/"+org+"/rooms/";
-    // console.log(url);
     var addRoomRef = new Firebase(url);
     addRoomRef.child(roomname).set('this room is empty');
   };
