520ddebe9d8a79ee8f483758ef6f56c7a9916342
RichardStanley <rstanley943@gmail.com>
2015-07-30 18:28:53 -0700

    (fix) Cleaning and commenting code

--- client/app/factories/oAuthFactory.js
+++ client/app/factories/oAuthFactory.js
@@ -3,19 +3,13 @@ angular.module('oAuthFactories', ['firebase'])
 .factory('oAuth',['$http', '$firebaseAuth', 'Users', function ($http, $firebaseAuth, $firebaseArray, Users){
 
   var oauthFactory = {};
-  // var ref = new Firebase('https://bizgramer.firebaseio.com/');
 
-  // var authObj = $firebaseAuth(ref);
   oauthFactory.oAuthIntuit = function($id, org) {
-    //get $id from $rootScope, look up user
-    //put token on user entry
     var users = Users.getUsers(org);
     var index = users.$indexFor($id);
-    // users[index].token;
-    // users[index].tokenSecret;
-    // users[index].realmId;
     users.$save(index);
   };
+  
   oauthFactory.getData = function(org){
     $http.get('/payable',{org:org}).success(function(){
       console.log('success');
@@ -23,7 +17,7 @@ angular.module('oAuthFactories', ['firebase'])
     $http.get('/receivable',{org:org}).success(function(){
       console.log('success');
     });
-    $http.get('/receivable',{org:org}).success(function(){
+    $http.get('/customers',{org:org}).success(function(){
       console.log('success');
     });
   };
